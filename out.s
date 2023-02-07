9b1db285969d5d902e7cf72567d607fa0537e317
Yuzhe710 <jackie2021ars@gmail.com>
2023-02-07 16:57:06 +0800

    more test

--- out.s
+++ out.s
@@ -20,35 +20,16 @@ printint:
 main:
 	pushq	%rbp
 	movq	%rsp, %rbp
-	movq	$12, %r8
-	movq	$3, %r9
-	imulq	%r8, %r9
-	movq	%r9, %rdi
-	call	printint
-	movq	$18, %r8
-	movq	$2, %r9
-	movq	$4, %r10
-	imulq	%r9, %r10
-	subq	%r10, %r8
-	movq	%r8, %rdi
-	call	printint
-	movq	$1, %r8
-	movq	$2, %r9
+	.comm	fred,8,8
+	.comm	jim,8,8
+	movq	fred(%rip), %r8
+	movq	(null), fred(%rip)
+	movq	fred(%rip), %r8
+	movq	(null), fred(%rip)
+	movq	fred(%rip), %r8
+	movq	fred(%rip), %r9
 	addq	%r8, %r9
-	movq	$9, %r8
-	addq	%r9, %r8
-	movq	$5, %r9
-	movq	$2, %r10
-	movq	%r9, %rax
-	cqo
-	idivq	%r10
-	movq	%rax,%r9
-	subq	%r9, %r8
-	movq	$3, %r9
-	movq	$5, %r10
-	imulq	%r9, %r10
-	addq	%r8, %r10
-	movq	%r10, %rdi
+	movq	%r9, %rdi
 	call	printint
 	movl	$0, %eax
 	popq	%rbp
